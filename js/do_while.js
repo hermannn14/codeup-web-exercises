1ef177255387913b08309731392795588276055f
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-06-07 17:32:03 -0500

    Loops exercises updated with do while loop

--- js/do_while.js
+++ js/do_while.js
@@ -1,13 +1,5 @@
 'use strict';
 
-//alert(*while loop*);
-
-var i = 1;
-while (i<65536) {
-    i = (i * 2)
-    console.log(i)
-}
-
 //alert (*do while loop*);
 
 // This is how you get a random number between 50 and 100
@@ -18,10 +10,11 @@ let total_cones = 0
 do {
     // This expression will generate a random number between 1 and 5
     let cones_bought = Math.floor(Math.random() * 5) + 1;
-    document.write("They bought" + cones_bought);
-    let total_cones = total_cones + cones_bought;
+    if ((allCones - total_cones) < cones_bought)
+    continue;
+    document.write("The client bought: " + cones_bought + "<br>");
+    total_cones = total_cones + cones_bought;
     document.write("I have sold: " + total_cones);
-    }
-    while (allCones <= total_cones );
-
 }
+while (allCones > total_cones );
+
