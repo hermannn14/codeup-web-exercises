84f740668e10281ce1a39fcc11a9ce980b69a35b
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-07-07 09:24:16 -0500

    July 7th warm up exercise

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -31,9 +31,45 @@ var names = ["Jean", "jacques", "rousseau"]
 console.log(names.toUpperCase);
 // Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
 // TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
-function capitalizeFirstLetter([]) {
-    var
-}
+// function capitalizeFirstLetter([]) {
+//     var
+// }
 
 //  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
 // Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+
+// July 7th 2021
+// TODO: Create a method on the object down below named getFullName that when called returns the users full name using the "this" key term.
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+function getFullName() {
+    var personOne = {
+        firstName: "silvia",
+        lastName: "floopertan",
+        ageInYears: 34,
+        heightInInches: 65
+    }
+    return (personOne.firstName + " " + personOne.lastName)
+}
+console.log(getFullName());
+// TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+//  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+function getNicelyFormattedFullName(value) {
+    var personTwo = {
+        firstName: "dan",
+        lastName: "valdarez",
+        ageInYears: 61,
+        heightInInches: 73
+    }
+    return (value[0].toUpperCase(personTwo.firstName) + " " + value[0].toUpperCase(personTwo.lastName))
+}
+console.log(getNicelyFormattedFullName(value))
+// TODO: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+    firstName: "arlene",
+    lastName: "martinez",
+    ageInYears: 25,
+    heightInInches: 62
+}
