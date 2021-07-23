38ef1f4a7999d137ab591c4e0b20d75cbadbc8a9
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-07-23 10:15:06 -0500

    Map Filter Reduce complete

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,3 +39,64 @@ const users = [
 console.log(users.filter(function(n) {
     return n.languages.length >= 3
 }))
+
+
+let emailAddresses = users.map(function (user) {
+    return user.email
+});
+console.log(emailAddresses);
+
+
+let totalYearsExp = users.reduce(function (accumulator, user) {
+    return accumulator + user.yearsOfExperience
+},0);
+console.log(totalYearsExp);
+
+let averageExp = totalYearsExp / users.length;
+
+console.log(averageExp);
+
+let longestEmail = users.reduce(function (currentLongest, user){
+    if (user.email.length > currentLongest.email.length) {
+        return user;
+    } else {
+        return currentLongest;
+    }
+}, users[0]);
+
+console.log(longestEmail.email);
+
+
+let nameString = users.reduce(function(accumulator, user){
+    if (accumulator === '') {
+        return "Your instructors are: " + user.name
+    } else {
+        return accumulator + ', ' + user.name
+    }
+}, '')
+
+console.log(nameString + ".");
+
+let concatLangs = users.reduce(function (accumulator, user){
+    return accumulator.concat(user.languages);
+}, []);
+
+console.log(concatLangs);
+
+// let uniqueLang = concatLangs.reduce(function (accumulator, currentValue){
+//     if(accumulator.indexOf(currentValue) === -1) {
+//         accumulator.push(currentValue);
+//     }
+//     return accumulator
+// }, [])
+
+let uniqueLang = concatLangs.reduce(function (accumulator, currentValue){
+    if(accumulator.includes(currentValue)) {
+        return accumulator;
+    } else {
+        accumulator.push(currentValue);
+        return accumulator
+    }
+}, [])
+
+console.log(uniqueLang)
