4db6da5a8d84a7c34939df9ef607a77bf6f135fe
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-07-24 19:20:53 -0500

    Promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,4 @@
-// fetch(url, {headers: {'Authorization': 'gitHubKey'}})
+fetch(url, {headers: {'Authorization': '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'}})
 
 function getGithubUsernames() {
     return fetch('https://api.github.com/users/'+userName+"/events/public",
@@ -8,7 +8,10 @@ function getGithubUsernames() {
         });
 }
 
-    getUsersFromGithubAsJSON("Herman-M-Bimikwe")
+    getUsersFromGithubAsJSON("Hermannn14")
         .then(function (userData) {
             console.log(userData[0])
         });
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
